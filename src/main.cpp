073b951cb1badac980f881215fe176947d05c4f3
Aneurin Toomey <ceanncliste@gmail.com>
2024-04-06 14:59:21 -0600

    Working Code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <SensorDefinitions.h>
 
 MF::DAQLine<CAN1> DAQLine;
-int prevTime = 0;
+int prevTime1 = 0;
+int prevTime2 = 0;
 
 void setup() {
   Serial.begin(9600); 
@@ -13,7 +14,7 @@ void setup() {
   DAQLine.begin();
   DAQLine.SDLoggingMode();
   DAQLine.enableDynamicSensors();
-  DAQLine.enableLiveTelemetry(Serial2);
+  //DAQLine.enableLiveTelemetry(Serial2);
 
   DAQLine.addSensor(1280, BATTERY_STATUS, 0);
   DAQLine.addSensor(280, ENGINE_STATUS, 0);
@@ -27,8 +28,13 @@ void setup() {
 void loop() {
   DAQLine.update();
 
-  if (millis()-prevTime > 10000){
+  if (millis()-prevTime1 > 5000){
     MF::SensorFactory::sendReadOut(Serial2);
-    prevTime = millis();
+    prevTime1 = millis();
+  }
+
+  if (millis()-prevTime2 > 250){
+    DAQLine.sendReadOut(Serial2);
+    prevTime2 = millis();
   }
 }
