2b1b50ba04c66ce26e7016a53e6ec058d4158ea9
Aneurin Toomey <ceanncliste@gmail.com>
2024-04-27 18:28:48 -0600

    Working Live

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #include <ECUSensors.hpp>
 #include <SensorDefinitions.h>
 
-MF::DAQLine<CAN1> DAQLine;
+MF::DAQLine<CAN1> ECULine;
+MF::DAQLine<CAN2> DAQLine;
 int prevTime1 = 0;
 int prevTime2 = 0;
 
 void setup() {
-  Serial.begin(9600); 
+  //Serial.begin(9600); 
   Serial2.begin(57600);
+  SD.begin(BUILTIN_SDCARD);
 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
@@ -19,26 +21,36 @@ void setup() {
   digitalWrite(3, 1);
   digitalWrite(4, 1);
 
-  DAQLine.begin();
+  ECULine.addSensor(1280, BATTERY_STATUS, 0);
+  ECULine.addSensor(280, ENGINE_STATUS, 0);
+  ECULine.addSensor(281, GEAR_STATUS, 0);
+  ECULine.addSensor(1284, PUMP_STATUS, 0);
+  ECULine.addSensor(1600, THROTTLE_STATUS, 0);
+  ECULine.addSensor(1604, ENGINE_RUNTIME, 0);
+  ECULine.addSensor(1609, ENGINE_TEMPERATURE, 0);
+  ECULine.addSensor(1621, BRAKE_STATUS, 0);
+
+  DAQLine.addSensor(1, ACCELEROMETER, 0);
+  DAQLine.addSensor(2, GYROSCOPE, 0);
+
+  ECULine.SDLoggingMode();
+  ECULine.enableLiveTelemetry(Serial2);
+
   DAQLine.SDLoggingMode();
   DAQLine.enableDynamicSensors();
   DAQLine.enableLiveTelemetry(Serial2);
 
-  DAQLine.addSensor(1280, BATTERY_STATUS, 0);
-  DAQLine.addSensor(280, ENGINE_STATUS, 0);
-  DAQLine.addSensor(281, GEAR_STATUS, 0);
-  DAQLine.addSensor(1284, PUMP_STATUS, 0);
-  DAQLine.addSensor(1600, THROTTLE_STATUS, 0);
-  DAQLine.addSensor(1604, ENGINE_RUNTIME, 0);
-  DAQLine.addSensor(1609, ENGINE_TEMPERATURE, 0);
-  DAQLine.addSensor(1621, BRAKE_STATUS, 0);
+  ECULine.begin();
+  DAQLine.begin();
 }
 
 void loop() {
+  ECULine.update();
   DAQLine.update();
 
   if (millis()-prevTime1 > 5000){
     MF::SensorFactory::sendReadOut(Serial2);
+    ECULine.flushSD();
     DAQLine.flushSD();
     prevTime1 = millis();
   }
